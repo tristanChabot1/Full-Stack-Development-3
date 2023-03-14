dfaca0f19fe5263815434b9990f87264e8c75e15
tristanChabot1 <tristanchabot10@gmail.com>
2023-03-14 13:56:30 -0400

    create modal done

--- mern/client/src/components/edit.js
+++ mern/client/src/components/edit.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
-import { useSubmit } from "react-router-dom";
 import ModalReact from './modal';
 
 
